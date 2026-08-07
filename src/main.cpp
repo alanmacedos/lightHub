22df5456849cedbbb3c0c111977697bab5a0afec
alanmacedos <alan.m.silva16@aluno.senai.br>
2026-08-07 17:11:07 -0300

    esp conection to firebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,85 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "S23 de Erick"
+#define WIFI_PASSWORD "12345678"
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyAIiRGxL6d5hHzpSKRvQ9xxvyZQOMtPhEU"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "https://esp32-lampada-default-rtdb.firebaseio.com/" 
+
+/* 4. Define the user Email and password that already added in your project */
+#define USER_EMAIL "esp-code@gmail.com"
+#define USER_PASSWORD "alauinha"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
 
 const int ledPin = 15;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  //  WiFi reconnection enabled
+  Firebase.reconnectNetwork(true);
+
+  // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+  Firebase.begin(&config, &auth);
+  Firebase.setDoubleDigits(5);
+  config.timeout.serverResponse = 10 * 1000;
 }
 
-void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(2000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
+void loop()
+{
+  // Firebase.ready() should be called repeatedly to handle authentication tasks.
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+
+  int ledState;
+   if(Firebase.RTDB.getInt(&fbdo, "/led/state", &ledState)){
+    digitalWrite(ledPin, ledState);
+   }else{
+    Serial.println(fbdo.errorReason().c_str());
+   }
+  }
 }
